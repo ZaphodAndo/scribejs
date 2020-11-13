f10e4f81e405d6a1c4ad050e62244c415bea12ed
Ethan Anderson <ethan.anderson00@gmail.com>
2020-11-13 21:28:43 +0000

    Added generate and randomiser functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
-const generate = (sentence) => {
-  const item = sentence.match(/\?(.*?)\?/gm);
-  console.log(item);
+const randomiser = (dataset) => {
+  const numOfValues = dataset.length;
+  const selectedValue = Math.floor(Math.random() * Math.floor(numOfValues));
+  const result = dataset[selectedValue];
+
+  return result;
 };
 
-export default generate;
+// Needs to be able to work out which dataset to get to pass into randomiser
+const generate = (sentence) => {
+  const words = sentence.split(" ");
+  let result = "";
+
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    if (word.match(/\?(.*?)\?/gm)) {
+      word = randomiser();
+    }
+    result = result + " " + word;
+  }
+
+  return result;
+};
+
+export { generate, randomiser };
