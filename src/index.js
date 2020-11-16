d53361d452db5f5acdee51753a3366d3f9bd27e9
Ethan Anderson <ethan.anderson00@gmail.com>
2020-11-16 21:42:26 +0000

    Converted Scribe to a class

--- src/index.js
+++ src/index.js
@@ -1,25 +1,42 @@
-const randomiser = (dataset) => {
-  const numOfValues = dataset.length;
-  const selectedValue = Math.floor(Math.random() * Math.floor(numOfValues));
-  const result = dataset[selectedValue];
+// Fix issue with grammar messing up the parsing.
 
-  return result;
-};
+class Scribe {
+  _datasets;
 
-// Needs to be able to work out which dataset to get to pass into randomiser
-const generate = (sentence) => {
-  const words = sentence.split(" ");
-  let result = "";
-
-  for (let i = 0; i < words.length; i++) {
-    let word = words[i];
-    if (word.match(/\?(.*?)\?/gm)) {
-      word = randomiser();
-    }
-    result = result + " " + word;
+  constructor(datasets) {
+    this._datasets = datasets;
   }
 
-  return result;
-};
+  randomiser(dataset) {
+    const numOfValues = dataset.length;
+    const selectedValue = Math.floor(Math.random() * Math.floor(numOfValues));
+    const result = dataset[selectedValue];
 
-export { generate, randomiser };
+    return result;
+  }
+
+  generator(sentence) {
+    const words = sentence.split(" ");
+    let result = "";
+
+    for (let i = 0; i < words.length; i++) {
+      let word = words[i];
+
+      if (word.match(/\|(.*?)\|/gm)) {
+        const selectedDataset = word.replace(/\|/g, "");
+
+        for (const [key, value] of Object.entries(this._datasets)) {
+          if (selectedDataset === key) {
+            word = this.randomiser(value);
+          }
+        }
+      }
+
+      result = result + " " + word;
+    }
+
+    return result;
+  }
+}
+
+export default Scribe;
